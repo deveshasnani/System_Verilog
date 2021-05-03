be52e80fe31a48a89a64ce43367cec88bad3f956
deveshasnani <77734571+deveshasnani@users.noreply.github.com>
2021-05-03 13:01:17 +0530

    bug fix

--- SPI Slave/design.sv
+++ SPI Slave/design.sv
@@ -1,3 +1,4 @@
+
 module spi_slave( 
   input SCLK,MOSI,SS,
   input [7:0] din,
@@ -17,13 +18,28 @@ module spi_slave(
   always @(SCLK)
     begin
       if(SCLK ==1) begin//posedge
-        if(mode[0] == 0) memory <= {MOSI , memory[7:1]};
-        else misoreg = memory[0];
+        case(mode)
+        0 :   memory <= {MOSI , memory[7:1]};
+        1 :   misoreg = memory[0];
+        2 :   misoreg = memory[0];
+        3 :   memory <= {MOSI , memory[7:1]};
+          
+        endcase
+        
+        // if(mode == 0) memory <= {MOSI , memory[7:1]};
+       // else misoreg = memory[0];
             end
             
       else if(SCLK == 0) begin  //negedge
-              if(mode[0] == 0) misoreg = memory[0];
-          else memory <= {MOSI , memory[7:1]};
+        case(mode)
+        3 :   misoreg = memory[0];
+        2 :   memory <= {MOSI , memory[7:1]};
+        1 :   memory <= {MOSI , memory[7:1]};
+        0 :   misoreg = memory[0];
+          
+        endcase  
+        //    if(mode[0] == 0) misoreg = memory[0];
+          //else memory <= {MOSI , memory[7:1]};
               
             end
     end
